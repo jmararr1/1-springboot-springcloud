a59695e5daf493a6b12e4ca8f0e6cf925e8fdad9
jmararr1 <marco.arraez.jorge@gmail.com>
2022-07-18 17:37:31 +0200

    termiando

---
+++ jorgeservices/customer/src/main/java/com/jorge/customer/CustomerService.java
@@ -0,0 +1,12 @@
+package com.jorge.customer;
+
+public record CustomerService (CustomerRepository customerRepository) {
+    public void registerCustomer (CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        customerRepository().save(customer);
+    }
+}
